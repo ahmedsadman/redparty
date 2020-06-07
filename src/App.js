132e363039bd4df477916575b0f8fba66b9fbe1d
Ahmed Sadman <ahmedsadman.211@gmail.com>
2020-06-07 20:30:33 +0600

    added sticky footer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Visible } from 'react-grid-system';
 import Room from './containers/Room';
 import Welcome from './containers/Welcome';
+import Footer from './components/common/Footer';
 
 class App extends React.Component {
 	render() {
 		return (
 			<Router>
-				<Switch>
-					<Route path='/' component={Welcome} exact />
-					<Route path='/room/:id' component={Room} exact />
-				</Switch>
+				<div style={{ minHeight: '100%' }}>
+					<Switch>
+						<Route path='/' component={Welcome} exact />
+						<Route path='/room/:id' component={Room} exact />
+					</Switch>
+					<Visible xs>
+						{/* Sticky footer Overlap fix for smaller screens */}
+						<div style={{ marginBottom: '60px' }}></div>
+					</Visible>
+				</div>
+				<Footer />
 			</Router>
 		);
 	}
---
+++ src/components/common/Footer.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+import { colors } from '../../config/colors';
+
+const Footer = (props) => (
+	<StyledFooter>
+		Crafted with&nbsp;
+		<ion-icon
+			name='heart-outline'
+			style={{ color: colors.primaryColor, fontSize: '1.3em' }}
+		></ion-icon>
+		&nbsp;by&nbsp;
+		<a
+			href='https://github.com/ahmedsadman'
+			target='_blank'
+			rel='noopener noreferrer'
+		>
+			ahmedsadman
+		</a>
+		&nbsp;(Source at&nbsp;
+		<a
+			href='https://github.com/ahmedsadman/redparty'
+			target='_blank'
+			rel='noopener noreferrer'
+		>
+			<ion-icon
+				name='logo-github'
+				style={{ fontSize: '1.3em' }}
+			></ion-icon>
+			)
+		</a>
+	</StyledFooter>
+);
+
+const StyledFooter = styled.div`
+	height: 40px;
+	margin-top: -40px;
+	display: flex;
+	flex: 1;
+	align-items: center;
+	justify-content: center;
+	font-size: 0.9em;
+	flex-wrap: wrap;
+	padding: 0 5%;
+	text-align: center;
+`;
+
+export default Footer;
