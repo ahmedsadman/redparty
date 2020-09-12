d9afab7f0de1de626ff78f72fa445fa4b963fba2
Ahmed Sadman <ahmedsadman.211@gmail.com>
2020-09-12 17:34:57 +0600

    moved styled components outside to avoid warnings

--- client/src/components/Room/Chat/Messages.js
+++ client/src/components/Room/Chat/Messages.js
@@ -3,47 +3,47 @@ import styled from 'styled-components';
 
 function Messages(props) {
 	return (
-		<MessageContainer>
+		<MessagesContainer>
 			<Message user='Jami'>Hello how are you?</Message>
 			<Message user='Samyo'>I am fine thank you</Message>
-		</MessageContainer>
+		</MessagesContainer>
 	);
 }
 
 const Message = (props) => {
-	const Container = styled.div`
-		display: flex;
-		flex-direction: column;
-		font-size: 0.9em;
-		margin-bottom: 10px;
-	`;
-
-	const User = styled.div`
-		font-weight: 800;
-		font-size: 0.85em;
-	`;
-
-	const Text = styled.p`
-		margin: 0;
-		padding: 0;
-		font-weight: 500;
-		font-size: 0.9em;
-	`;
-
 	return (
-		<Container>
+		<MessageContainer>
 			<User>{props.user}</User>
 			<Text>{props.children}</Text>
-		</Container>
+		</MessageContainer>
 	);
 };
 
-const MessageContainer = styled.div`
+const MessagesContainer = styled.div`
 	display: flex;
 	flex-direction: column;
 	flex: 1;
 	padding: 5px;
 	overflow-y: auto;
+`;
+
+const MessageContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	font-size: 0.9em;
+	margin-bottom: 10px;
+`;
+
+const User = styled.div`
+	font-weight: 800;
+	font-size: 0.85em;
+`;
+
+const Text = styled.p`
+	margin: 0;
+	padding: 0;
+	font-weight: 500;
+	font-size: 0.9em;
 `;
 
 export default Messages;
