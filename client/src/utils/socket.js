01eef0a2bf4c65113c7c5f784a269d686fc14ef9
Ahmed Sadman <ahmedsadman.211@gmail.com>
2020-09-14 00:32:47 +0600

    added .env variables

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 export const createConnection = (name, roomId = null) => {
 	// create the socket connection with socket server
 	return new Promise((resolve) => {
-		const socket = io('http://localhost:3005', { path: '/socket' });
+		const socket = io(process.env.REACT_APP_SERVER, { path: '/socket' });
 		socket.on('connect', () => {
 			socket.emit('join', {
 				roomId: roomId || socket.id,
