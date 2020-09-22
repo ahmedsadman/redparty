a4bd8dfedaece4e92f62ed07dc1d4f924303ceb0
Ahmed Sadman <ahmedsadman.211@gmail.com>
2020-09-22 20:24:14 +0600

    chat autoscroll based on scroll position

--- client/src/components/Room/Chat/Messages.js
+++ client/src/components/Room/Chat/Messages.js
@@ -1,38 +1,65 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, createRef } from 'react';
 import styled from 'styled-components';
 
 function Messages(props) {
-	let messageEnd = null;
+	let messageEnd = createRef();
+	let messagesContainer = createRef();
 	const { messages } = props;
 
 	const scrollToBottom = () => {
-		if (messageEnd) {
-			messageEnd.scrollIntoView({ behavior: 'smooth' });
+		if (!messagesContainer.current || !messageEnd.current) return;
+
+		const {
+			clientHeight,
+			scrollTop,
+			scrollHeight,
+		} = messagesContainer.current;
+
+		// totalMessage >= 1 always, because we have a dummy div as a permanent children
+		const totalMessage = messagesContainer.current.children.length;
+
+		const newMessage = messagesContainer.current.children[totalMessage - 2];
+		const lastMessage =
+			totalMessage > 2
+				? messagesContainer.current.children[totalMessage - 3]
+				: null;
+
+		if (!newMessage || !lastMessage) return;
+
+		const newMessageHeight = newMessage.clientHeight;
+		const lastMessageHeight = lastMessage.clientHeight;
+
+		// -15 for error margin
+		if (
+			clientHeight + scrollTop + newMessageHeight + lastMessageHeight >=
+			scrollHeight - 15
+		) {
+			messageEnd.current.scrollIntoView({ behavior: 'smooth' });
 		}
 	};
 
-	useEffect(() => scrollToBottom(), [messages]);
+	useEffect(scrollToBottom, [messages]);
 
 	return (
-		<MessagesContainer>
+		<MessagesContainer ref={messagesContainer}>
 			{messages.map((message) => (
 				<Message user={message.from} key={message.id}>
 					{message.text}
 				</Message>
 			))}
-			<div className='dummy' ref={(el) => (messageEnd = el)}></div>
+			<div className='dummy' ref={messageEnd}></div>
 		</MessagesContainer>
 	);
 }
 
 const Message = (props) => {
 	return props.user !== null ? (
-		<MessageContainer>
+		<MessageContainer className={props.className}>
 			<User>{props.user}</User>
 			<Text>{props.children}</Text>
 		</MessageContainer>
 	) : (
-		<MessageContainer adminMessage>
+		<MessageContainer adminMessage className={props.className}>
 			<Text>{props.children}</Text>
 		</MessageContainer>
 	);
@@ -43,6 +70,7 @@ const MessagesContainer = styled.div`
 	flex-direction: column;
 	flex: 1;
 	padding: 5px;
+	box-sizing: border-box;
 	overflow-y: auto;
 `;
 
@@ -51,6 +79,7 @@ const MessageContainer = styled.div`
 	flex-direction: column;
 	font-size: 0.9em;
 	margin-bottom: 10px;
+	box-sizing: border-box;
 
 	${({ adminMessage }) =>
 		adminMessage && { 'text-align': 'center', color: 'gray' }}
